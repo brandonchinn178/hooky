462e09708c853e6f27c4be9833d2c13deabb1800
Brandon Chinn <brandonchinn178@gmail.com>
2026-01-17 14:38:02 -0800

    Add github actions

--- test/Hooky/TestUtils/Hooky.hs
+++ test/Hooky/TestUtils/Hooky.hs
@@ -4,13 +4,16 @@ module Hooky.TestUtils.Hooky (
 
 import Skeletest
 import System.Directory (findExecutable)
+import System.Environment (lookupEnv)
 
 newtype HookyExe = HookyExe FilePath
 
 instance Fixture HookyExe where
   fixtureScope = PerSessionFixture
   fixtureAction = do
-    exe <-
+    exe <- lookupEnv "TEST_HOOKY_EXE" >>= maybe findExe pure
+    pure $ noCleanup (HookyExe exe)
+   where
+    findExe =
       findExecutable "hooky"
         >>= maybe (error "Could not find hooky executable") pure
-    pure $ noCleanup (HookyExe exe)
