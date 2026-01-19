d25b64f0ce71482a780e905663c7c6738ff4b171
Brandon Chinn <brandonchinn178@gmail.com>
2026-01-18 23:44:28 -0800

    Release v1.0.0

--- scripts/release_notes.py
+++ scripts/release_notes.py
@@ -2,10 +2,8 @@
 
 import argparse
 import itertools
-import json
-import subprocess
 from pathlib import Path
-from typing import Any, Mapping
+from typing import Mapping
 
 TOP = Path(__file__).resolve().parent.parent
 
@@ -49,26 +47,6 @@ def validate_changelog(version: str) -> None:
 def get_changelog_for(version: str) -> str:
     changelog = (TOP / "CHANGELOG.md").read_text().splitlines()
 
-    assert changelog[0] == "# Unreleased"
-    changelog = changelog[1:]
-    while len(changelog[0].strip()) == 0:
-        changelog = changelog[1:]
-
-    unreleased_changes = "\n".join(
-        itertools.takewhile(
-            lambda line: not line.startswith("#"),
-            changelog,
-        )
-    )
-    if unreleased_changes:
-        raise Exception(
-            f"""
-            CHANGELOG has unreleased changes:
-
-            {unreleased_changes}
-            """
-        )
-
     if changelog[0] != f"# v{version}":
         raise Exception(
             f"""
@@ -89,44 +67,8 @@ def get_changelog_for(version: str) -> str:
 def generate(version: str, binaries: Mapping[str, Path]) -> None:
     release_notes = [
         get_changelog_for(version),
-        "## [dotslash](https://dotslash-cli.com/docs/) config",
-        "```json",
-        "#!/usr/bin/env dotslash",
-        json.dumps(
-            get_dotslash_config(version, binaries),
-            indent=2,
-        ),
-        "```",
     ]
     print("\n".join(release_notes))
-
-
-def get_dotslash_config(version: str, binaries: Mapping[str, Path]) -> Any:
-    return {
-        "name": f"{BIN_NAME}-{version}",
-        "platforms": {
-            name: {
-                "size": bin_path.stat().st_size,
-                "hash": "sha256",
-                "digest": get_digest(bin_path),
-                "path": BIN_NAME,
-                "providers": [
-                    {"url": f"{BASE_DOWNLOAD_URL}/v{version}/{bin_path.name}"},
-                ],
-            }
-            for name, bin_path in binaries.items()
-        },
-    }
-
-
-def get_digest(bin_path: Path) -> str:
-    proc = subprocess.run(
-        ["shasum", "-a", "256", bin_path],
-        check=True,
-        text=True,
-        stdout=subprocess.PIPE,
-    )
-    return proc.stdout.split()[0]
 
 
 if __name__ == "__main__":
